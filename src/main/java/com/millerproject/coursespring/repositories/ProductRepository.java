2b678d3afc1fabb31299d864c3acf550cb81119b
MillerFelix <millerredigolo@gmail.com>
2023-11-27 17:09:27 -0300

    Product

---
+++ src/main/java/com/millerproject/coursespring/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.millerproject.coursespring.repositories;
+
+import com.millerproject.coursespring.entities.Category;
+import com.millerproject.coursespring.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
