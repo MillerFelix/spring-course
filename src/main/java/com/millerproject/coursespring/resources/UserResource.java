e59c5dc83b0eebe7263a2ebe022a5992f6cf48cf
MillerFelix <millerredigolo@gmail.com>
2023-11-28 10:24:02 -0300

    User insert

--- src/main/java/com/millerproject/coursespring/resources/UserResource.java
+++ src/main/java/com/millerproject/coursespring/resources/UserResource.java
@@ -4,8 +4,11 @@ import com.millerproject.coursespring.entities.User;
 import com.millerproject.coursespring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,5 +28,12 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
