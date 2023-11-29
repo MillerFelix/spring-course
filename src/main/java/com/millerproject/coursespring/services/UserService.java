a028e4af33867638b1033b161dd3ffcd6ce93ab1
MillerFelix <millerredigolo@gmail.com>
2023-11-29 08:51:38 -0300

    Exception handling - findById

--- src/main/java/com/millerproject/coursespring/services/UserService.java
+++ src/main/java/com/millerproject/coursespring/services/UserService.java
@@ -2,6 +2,7 @@ package com.millerproject.coursespring.services;
 
 import com.millerproject.coursespring.entities.User;
 import com.millerproject.coursespring.repositories.UserRepository;
+import com.millerproject.coursespring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
---
+++ src/main/java/com/millerproject/coursespring/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.millerproject.coursespring.services.exceptions;
+
+import java.io.Serial;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
