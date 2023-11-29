74acdc338867a2f2caab5177eef0fa399326bfa5
MillerFelix <millerredigolo@gmail.com>
2023-11-29 09:20:23 -0300

    Exception handling - update

--- src/main/java/com/millerproject/coursespring/services/UserService.java
+++ src/main/java/com/millerproject/coursespring/services/UserService.java
@@ -4,6 +4,7 @@ import com.millerproject.coursespring.entities.User;
 import com.millerproject.coursespring.repositories.UserRepository;
 import com.millerproject.coursespring.services.exceptions.DatabaseException;
 import com.millerproject.coursespring.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,9 +42,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
